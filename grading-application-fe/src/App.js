1e973875d7a2f4db8ac9bf3a13406b76ab4a4f86
sachintha97 <ssamarasiri12@gmail.com>
2020-08-04 00:58:25 +0530

    update: initial UI design

--- grading-application-fe/src/App.js
+++ grading-application-fe/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+import Assignments from "./components/AssignmentPage";
+import Review from "./components/Review";
+import Home from "./components/Home";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route component={Home} path="/" exact />
+          <Route component={Assignments} path="/assignments" exact/>
+          <Route component={Review} path="/assignments/:id" />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ grading-application-fe/src/components/NavBar.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { Link as RouterLink } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+
+  navbar: {
+    background: "#000066",
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  // create main nav bar
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My Assignment</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+          <Typography variant="h6" className={classes.title}>
+            GRADING TOOL
+          </Typography>
+          <Button
+            startIcon={<HomeIcon />}
+            color="inherit"
+            component={RouterLink}
+            to="/"
+          >
+            Home
+          </Button>
+          <Button
+            startIcon={<AssignmentIcon />}
+            color="inherit"
+            component={RouterLink}
+            to="/assignments"
+          >
+            Assignment
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
